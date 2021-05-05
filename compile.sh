74c189ea097aa7597e6675d2b51efd7520253d1c
Anton Moskalenko <moskalenko.anton@gmail.com>
2021-05-05 10:28:08 +0300

    Create compiler.

---
+++ compile.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+php compiler.php notes/secret-of-tonce.txt secret-of-tonce.txt
---
+++ compiler.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "\nText compiler\nVersion 0.1\nAnton (Liloi) Moskalenko\n\n";
+
+$inputFilename = __DIR__ . '/' . $argv[1];
+$outputFilename = __DIR__ . '/' . $argv[2];
+
